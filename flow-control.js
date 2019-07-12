7236ba26745dca9b3b5edfc2c8d326c7dd95eb86
george.burton <george.burton@wework.com>
2019-07-12 22:43:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,39 @@
 function basicTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!";
+ }
 }
 
-function teenager(age) {
 
+function teenager(age) {
+  if (age >= 13 && age <= 19) {
+   return "You are a teenager!"
+} else {
+  return "You are not a teenager"
+ }
 }
 
 function ageChecker(age) {
+  if(age >=13 && age <=19)
+  return "You are a teenager!" 
+  else if (age <= 12) {
+  return "You are a kid"
+} else if(age >=20) {
+  return "You are a grownup"
+}else 
+return "You are a dingleberry"
 
 }
 
 function ternaryTeenager(age) {
-
+return(age >=13 && age <=19) ? "You are a teenager":"You are not a teenager"
 }
-
+var = age = case(age)
 function switchAge(age) {
+case age (age >=): "You have an age" 
+    break;
+case(age <=13 || >= 19):
+console.log("You have an age")
 
 }
+
