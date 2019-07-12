b65818fb1d9565c5f5ce5c9f6afdc92f2379a1c7
george.burton <george.burton@wework.com>
2019-07-12 23:06:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,7 +30,7 @@ return(age >=13 && age <=19) ? "You are a teenager":"You are not a teenager"
 }
 function switchAge(age) {
   switch(age) {
-case 13,14,15,16,17,18,19: "You are ateenager";
+case 13,14,15,16,17,18,19: "You are a teenager";
    break;
    default:
    return "You have an age" 
