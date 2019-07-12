91604b2f004cf1173a6b137c5b3b541e0e1c55c0
george.burton <george.burton@wework.com>
2019-07-12 23:23:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -34,12 +34,9 @@ case 13 : return "You are a teenager";
 case 14 : return "You are a teenager";
 case 15 : return "You are a teenager";
 case 16 : return "You are a teenager";
-break;
-case 17 :"You are a teenager";
-case 18 :"You are a teenager";
-break;
-case 19 : "You are a teenager";
-break;
+case 17 : return"You are a teenager";
+case 18 :return"You are a teenager";
+case 19 :return "You are a teenager";
    default: "You have an age"; 
   } 
 }
